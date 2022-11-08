557e9d35bec0b64dc7204d7e1f4529ee8413ab84
Dineshkumar <dinesh@increatech.com>
2022-11-08 17:02:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <!DOCTYPE html>
 <html><head></head><body>
 <?php
-while (list($var,$value) = each ($_ENV)) {
-    echo "$var => $value <br />";
+$env_array =getenv();
+
+echo "<h3>The list of environment variables with values are :</h3>";
+
+//Print all environment variable names with values
+
+foreach ($env_array as $key=>$value)
+
+{
+
+    echo "$key => $value <br />";
+
 }
+
 ?>
 </body></html>
